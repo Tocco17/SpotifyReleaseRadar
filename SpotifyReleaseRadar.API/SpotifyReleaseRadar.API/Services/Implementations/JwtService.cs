8281bb235971f24a1e7c873a469a600621249aa2
FedericoRuocco <fede.ruocco@gmail.com>
2025-05-07 17:23:36 +0200

    api

--- SpotifyReleaseRadar.API/SpotifyReleaseRadar.API/Services/Implementations/JwtService.cs
+++ SpotifyReleaseRadar.API/SpotifyReleaseRadar.API/Services/Implementations/JwtService.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
-using System.Security.Claims;
-using System.Text;
-
-using SpotifyReleaseRadar.API.ControllerModels.UserControllers.Requests;
+﻿using SpotifyReleaseRadar.API.ControllerModels.UserControllers.Requests;
 using SpotifyReleaseRadar.API.ControllerModels.UserControllers.Responses;
 using SpotifyReleaseRadar.API.Services.Interfaces;
+using SpotifyReleaseRadar.API.Utilities;
 
 namespace SpotifyReleaseRadar.API.Services.Implementations;
 
@@ -16,13 +13,11 @@ public class JwtService : IJwtService
 		var spotifyClientId = Environment.GetEnvironmentVariable("SpotifyClientId");
 		var spotifyClientSecret = Environment.GetEnvironmentVariable("SpotifyClientSecret");
 
-		var response = APICall.Post<LoginResponse>(spotifyAuthTokenUrl, new
+		var response = ApiCall.Post<LoginResponse>(spotifyAuthTokenUrl, new
 		{
 			grant_type = "authorization_code",
-			code = req.Code,
-			redirect_uri = req.RedirectUri,
 			client_id = spotifyClientId,
-			client_secret = spotifyClientSecret
+			client_secret = spotifyClientSecret,
 		});
 
 		return response;
