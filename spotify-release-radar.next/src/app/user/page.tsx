4c53754baa5adcbcfcc8b7afbd2aaff5f5d5d191
FedericoRuocco <fede.ruocco@gmail.com>
2025-05-08 15:18:21 +0200

    check and test

--- spotify-release-radar.next/src/app/user/page.tsx
+++ spotify-release-radar.next/src/app/user/page.tsx
@@ -1,6 +1,19 @@
 'use server'
 
+import { getCurrentUserProfile } from "@/lib/spotifyCalls/getCurrentUserProfile"
+
+async function getUserData(){
+	const userData = await getCurrentUserProfile()
+
+	console.log('userData')
+	console.log(userData)
+	
+	return userData
+}
+
 export default async function UserPage() {
+	const userData = await getUserData()
+	
 	return (<>
 		Hi
 	</>)
