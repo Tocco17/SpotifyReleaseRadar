3b0586c443021969da9300a5a66ebea265adc433
FedericoRuocco <fede.ruocco@gmail.com>
2025-05-09 18:00:40 +0200

    shadcn avatar

--- spotify-release-radar.next/src/app/user/page.tsx
+++ spotify-release-radar.next/src/app/user/page.tsx
@@ -1,17 +1,28 @@
 'use server'
 
 import { getCurrentUserProfile } from "@/lib/spotifyCalls/getCurrentUserProfile"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-async function getUserData(){
+
+async function getUserData() {
 	const userData = await getCurrentUserProfile()
+	console.log(userData)
 
 	return userData
 }
 
 export default async function UserPage() {
 	const userData = await getUserData()
-	
+
 	return (<>
-		Hi
+		<p>Welcome {userData.display_name}</p>
+		{
+			userData.images && userData.images[0]?.url &&
+			<Avatar>
+				<AvatarImage src={userData.images[0].url} />
+				<AvatarFallback>CN</AvatarFallback>
+			</Avatar>
+		}
+
 	</>)
 }
