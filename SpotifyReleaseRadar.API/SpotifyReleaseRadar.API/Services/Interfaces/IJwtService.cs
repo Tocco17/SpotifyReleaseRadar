9d47f7e9ac62e4e36a568c0a1dd1959c2a00cd6c
FedericoRuocco <fede.ruocco@gmail.com>
2025-05-12 12:35:57 +0200

    clean of api

--- SpotifyReleaseRadar.API/SpotifyReleaseRadar.API/Services/Interfaces/IJwtService.cs
+++
@@ -1,9 +0,0 @@
-﻿using SpotifyReleaseRadar.API.ControllerModels.UserControllers.Requests;
-using SpotifyReleaseRadar.API.ControllerModels.UserControllers.Responses;
-
-namespace SpotifyReleaseRadar.API.Services.Interfaces;
-
-public interface IJwtService
-{
-	Task<LoginResponse?> Login(LoginRequest req);
-}
